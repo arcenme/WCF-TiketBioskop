0a13fe866d0fe05bb98d0a506fd8177c84411d55
Ari Suseno <arcen.grizzly@gmail.com>
2018-12-05 11:14:47 +0700

    add references and configure server

--- ServiceTB/BioskopService.cs
+++ ServiceTB/BioskopService.cs
@@ -11,7 +11,7 @@ using System.Globalization;
 namespace ServiceTB
 {
     // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in both code and config file together.
-    public class ServiceTB : IServiceTB
+    public class BioskopService : IBioskopService
     {
       
     }
