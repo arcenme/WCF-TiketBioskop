19db6fdd32ad39adc903d1933b18a88de7bcc560
Ari Suseno <arcen.grizzly@gmail.com>
2018-12-05 21:24:14 +0700

    BioskoAdmin 80%

--- AdminTB/01TambahBioskop.cs
+++ AdminTB/01TambahBioskop.cs
@@ -15,6 +15,8 @@ namespace AdminTB
 {
     public partial class _01TambahBioskop : Form
     {
+        string BaseAddress = "http://localhost:8080/";
+        string _IDB;
         public _01TambahBioskop()
         {
             InitializeComponent();
@@ -22,7 +24,10 @@ namespace AdminTB
 
         private void _01TambahBioskop_Load(object sender, EventArgs e)
         {
-
+           
+            panelBioskop.Visible = true;
+            panelDataBioskop.Visible = false;
+            
         }
 
         private void TambahBioskop_Click(object sender, EventArgs e)
@@ -32,12 +37,55 @@ namespace AdminTB
 
         private void EditBioskop_Click(object sender, EventArgs e)
         {
+            try
+            {
+                
+                panelBioskop.Visible = false;
+                panelDataBioskop.Visible = true;
+                
+                //Get Data
+                var listbskp = listViewBioskop.SelectedItems[0];
+                string IDBioskop = listbskp.SubItems[1].Text;
+                string NamaBioskop = listbskp.SubItems[2].Text;
+                string AlamatBisokop = listbskp.SubItems[3].Text;
+                string Jumlah_Kursi = listbskp.SubItems[4].Text;
+                _IDB = listbskp.SubItems[0].Text;
 
+                panelDataBioskop.Visible = false;
+                panelEditBioskop.Visible = true;
+
+                EditIDBskp.AppendText(IDBioskop);
+                EditNamaBskp.AppendText(NamaBioskop);
+                EditAlamatBskp.AppendText(AlamatBisokop);
+                EditKapasitasBskp.AppendText(Jumlah_Kursi);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Chose Data", "Show Data", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
         }
 
         private void HapusBioskop_Click(object sender, EventArgs e)
         {
+            try
+            {
+                var listbskp = listViewBioskop.SelectedItems[0];
+                string IDBioskop = listbskp.SubItems[0].Text;
 
+                DataBioskop DBBskp = new DataBioskop();
+                DBBskp.IDBioskop = IDBioskop;
+
+                var dataPut = JsonConvert.SerializeObject(DBBskp);
+                var Json = new WebClient();
+                Json.Headers.Add(HttpRequestHeader.ContentType, "application/json; charset=utf-8");
+                string response = Json.UploadString(BaseAddress + "Bioskop/Delete", "DELETE", dataPut);
+                Console.WriteLine(response);
+                MessageBox.Show("Sukses", "Hapus Data", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Hapus Data", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
         }
 
         private void kembaliBioskop_Click(object sender, EventArgs e)
@@ -57,10 +105,100 @@ namespace AdminTB
 
         private void KonfirmasiBioskop_Click(object sender, EventArgs e)
         {
+            try
+            {
+                DataBioskop DBBskp = new DataBioskop();
+                DBBskp.IDBioskop = textBoxIDBioskop.Text;
+                DBBskp.NamaBioskop = textBoxNamaBioskop.Text;
+                DBBskp.AlamatBioskop = textBoxAlamatBioskop.Text;
+                DBBskp.Jumlah_Kursi = Int32.Parse(CmBoxKapasitas.Text);
 
+                //Not Null
+                if (!DBBskp.IDBioskop.Equals("") || DBBskp.NamaBioskop.Equals("") || DBBskp.AlamatBioskop.Equals("") || DBBskp.Jumlah_Kursi.Equals(""))
+                {
+                    var dataBioskop = JsonConvert.SerializeObject(DBBskp);
+                    var JsonBioskop = new WebClient();
+                    JsonBioskop.Headers.Add(HttpRequestHeader.ContentType, "application/json; charset=utf-8");
+                    string responseBioskop = JsonBioskop.UploadString(BaseAddress + "Bioskop", dataBioskop);
+                    Console.WriteLine(responseBioskop);
+
+                    MessageBox.Show("Sukses", "Create Data", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    textBoxIDBioskop.Text = "";
+                    textBoxNamaBioskop.Text = "";
+                    textBoxAlamatBioskop.Text = "";
+                    CmBoxKapasitas.Text = "";
+                }
+                else
+                {
+                    MessageBox.Show("Insert Data", "Create Data", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Create Data", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
         }
 
         private void BatalBioskop_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void LihatDataBioskop_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                panelBioskop.Visible = false;
+                panelDataBioskop.Visible = true;
+                panelEditBioskop.Visible = false;
+
+                listViewBioskop.Items.Clear();
+                var json = new WebClient().DownloadString(BaseAddress + "Bioskop");
+                var data = JsonConvert.DeserializeObject<List<DataBioskop>>(json);
+                foreach (var bskp in data)
+                {
+                    ListViewItem dataBioskop = new ListViewItem(Convert.ToString(bskp.IDB));
+                    dataBioskop.SubItems.Add(bskp.IDBioskop);
+                    dataBioskop.SubItems.Add(bskp.NamaBioskop);
+                    dataBioskop.SubItems.Add(bskp.AlamatBioskop);
+                    dataBioskop.SubItems.Add(Convert.ToString(bskp.Jumlah_Kursi));
+                    listViewBioskop.Items.Add(dataBioskop);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Show Data", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void KonfirmasiEditBskp_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                DataBioskop DBBskp = new DataBioskop();
+                DBBskp.IDBioskop = EditIDBskp.Text;
+                DBBskp.NamaBioskop = EditNamaBskp.Text;
+                DBBskp.AlamatBioskop = EditAlamatBskp.Text;
+                DBBskp.Jumlah_Kursi = Int32.Parse(EditKapasitasBskp.Text);
+                var dataPut = JsonConvert.SerializeObject(DBBskp);
+                var Json = new WebClient();
+                Json.Headers.Add(HttpRequestHeader.ContentType, "application/json; charset=utf-8");
+                string response = Json.UploadString(BaseAddress + "Bioskop/Edit", "PUT", dataPut);
+                Console.WriteLine(response);
+                MessageBox.Show("Sukses", "Edit Data", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Edit Data", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
         {
 
         }
--- AdminTB/DataBioskop.cs
+++ AdminTB/DataBioskop.cs
@@ -3,10 +3,59 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Runtime.Serialization;
 
 namespace AdminTB
 {
-    class DataBioskop
+    [DataContract]
+    public class DataBioskop
     {
+        [DataMember]
+        public int IDFilm { get; set; }
+
+        [DataMember]
+        public int IDB { get; set; }
+
+        [DataMember]
+        public string IDBioskop { get; set; }
+
+        [DataMember]
+        public string NamaBioskop { get; set; }
+
+        [DataMember]
+        public string AlamatBioskop { get; set; }
+
+        [DataMember]
+        public string JudulFilm { get; set; }
+
+        [DataMember]
+        public string Aktor { get; set; }
+
+        [DataMember]
+        public string Ratting { get; set; }
+
+        [DataMember]
+        public string Ringkasan { get; set; }
+
+        [DataMember]
+        public string Tanggal { get; set; }
+
+        [DataMember]
+        public string Jam { get; set; }
+
+        [DataMember]
+        public string Nomor_Kursi { get; set; }
+
+        [DataMember]
+        public int Harga { get; set; }
+
+        [DataMember]
+        public int Jumlah_Kursi { get; set; }
+
+        [DataMember]
+        public string Ketersediaan { get; set; }
+
+        [DataMember]
+        public string Poster { get; set; }
     }
 }
