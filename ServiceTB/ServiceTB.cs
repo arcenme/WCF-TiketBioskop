39b8d7dfe2f0ced20267e8645d51fa6658f4d725
Ari Suseno <arcen.grizzly@gmail.com>
2018-12-05 10:47:34 +0700

    add references

---
+++ ServiceTB/ServiceTB.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+using System.Data.SqlClient;
+using System.Collections;
+using System.Globalization;
+
+namespace ServiceTB
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in both code and config file together.
+    public class ServiceTB : IServiceTB
+    {
+      
+    }
+}
