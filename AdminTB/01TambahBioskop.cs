0dce4da45bda8e14081c0d6f96e2bac7cb2058e7
Ari Suseno <arcen.grizzly@gmail.com>
2018-12-06 20:02:45 +0700

    Layout Edit Film

--- AdminTB/01TambahBioskop.cs
+++ AdminTB/01TambahBioskop.cs
@@ -232,5 +232,15 @@ namespace AdminTB
         {
 
         }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
