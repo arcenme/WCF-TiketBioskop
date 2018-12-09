85624349abd62904c0a49b3b74860fd24ba3756a
Ari Suseno <arcen.grizzly@gmail.com>
2018-12-09 22:17:58 +0700

    code input data ke database

--- AdminTB/01TambahBioskop.cs
+++ AdminTB/01TambahBioskop.cs
@@ -348,7 +348,50 @@ namespace AdminTB
 
         private void buttonInput_Click(object sender, EventArgs e)
         {
+            try
+            {
+                panelBioskop.Visible = false;
+                panelDataBioskop.Visible = false;
+                panelDataFilm.Visible = false;
+                panelEditBioskop.Visible = false;
+                panelEditFilm.Visible = false;
+                panelFilm.Visible = false;
+                panelJadwal.Visible = false;
+                panelKursi.Visible = false;
+                panelSign.Visible = true;
 
+                var jsonID = new WebClient().DownloadString(BaseAddress + "Bioskop/IDBioskop");
+                var dataID = JsonConvert.DeserializeObject<List<DataBioskop>>(jsonID);
+                comboBoxIDBioskop.Items.Clear();
+                foreach (var bskp in dataID)
+                {
+                    comboBoxIDBioskop.Items.Add(bskp.IDBioskop);
+                }
+
+                var json = new WebClient().DownloadString(BaseAddress + "Film");
+                var data = JsonConvert.DeserializeObject<List<DataBioskop>>(json);
+                comboBoxJudulFilm.Items.Clear();
+                foreach (var film in data)
+                {
+                    comboBoxJudulFilm.Items.Add(film.JudulFilm);
+                }
+
+                comboBoxJam.Items.Clear();
+                for (int x = 1; x <= 24; x++)
+                {
+                    comboBoxJam.Items.Add(x);
+                }
+
+                comboBoxMenit.Items.Clear();
+                for (int x = 1; x <= 60; x++)
+                {
+                    comboBoxMenit.Items.Add(x);
+                }
+            }
+            catch (Exception ex)
+            {
+
+            }
         }
 
         private void button7_Click(object sender, EventArgs e)
@@ -358,7 +401,198 @@ namespace AdminTB
 
         private void button8_Click(object sender, EventArgs e)
         {
+            try
+            {
 
+                _IDB = comboBoxIDBioskop.Text;
+                int jumlahKursi;
+                string jam = comboBoxJam.Text;
+                string menit = comboBoxMenit.Text;
+                string waktu = jam + ":" + menit;
+
+                var json = new WebClient().DownloadString(BaseAddress + "Bioskop/IDBioskop=" + _IDB);
+                DataBioskop bskp = JsonConvert.DeserializeObject<DataBioskop>(json);
+                jumlahKursi = bskp.Jumlah_Kursi;
+
+                DataBioskop DBInput = new DataBioskop();
+
+                DBInput.Ketersediaan = "Y";
+                DBInput.IDBioskop = _IDB;
+                DBInput.JudulFilm = comboBoxJudulFilm.Text;
+                DBInput.Tanggal = DateBulan.Text;
+                DBInput.Jam = waktu;
+
+                string huruf = "";
+                string[] kursi109 = new string[109];
+                string[] kursi188 = new string[188];
+                int count;
+
+                //Not Null
+                if (!DBInput.IDBioskop.Equals("") || DBInput.JudulFilm.Equals("") || DBInput.Tanggal.Equals("") || DBInput.Jam.Equals(""))
+                {
+                    if (jumlahKursi == 109)
+                    {
+                        count = 0;
+                        for (char c = 'A'; c <= 'J'; c++)
+                        {
+                            switch (c)
+                            {
+                                case 'A':
+                                    huruf = "A";
+                                    break;
+                                case 'B':
+                                    huruf = "B";
+                                    break;
+                                case 'C':
+                                    huruf = "C";
+                                    break;
+                                case 'D':
+                                    huruf = "D";
+                                    break;
+                                case 'E':
+                                    huruf = "E";
+                                    break;
+                                case 'F':
+                                    huruf = "F";
+                                    break;
+                                case 'G':
+                                    huruf = "G";
+                                    break;
+                                case 'H':
+                                    huruf = "H";
+                                    break;
+                                case 'I':
+                                    huruf = "I";
+                                    break;
+                                case 'J':
+                                    huruf = "J";
+                                    break;
+                            }
+
+                            for (int i = 1; i <= 12; i++)
+                            {
+                                if (c == 'I' || c == 'O') { break; }
+                                kursi109[count] = Convert.ToString(huruf + i);
+                                count++;
+                                if (c == 'A' && i == 12)
+                                {
+                                    kursi109[count] = Convert.ToString(huruf + (i + 1));
+                                    count++;
+                                }
+                            }
+                        }
+
+                        for (int x = 0; x < kursi109.Length; x++)
+                        {
+                            DBInput.Nomor_Kursi = kursi109[x];
+                            var data = JsonConvert.SerializeObject(DBInput);
+                            var Json = new WebClient();
+                            Json.Headers.Add(HttpRequestHeader.ContentType, "application/json; charset=utf-8");
+                            string response = Json.UploadString(BaseAddress + "Inputdata", data);
+                            Console.WriteLine(response);
+                        }
+                    }
+                    else if (jumlahKursi == 188)
+                    {
+                        count = 0;
+                        bool cek = true;
+                        for (char c = 'A'; c <= 'K'; c++)
+                        {
+                            switch (c)
+                            {
+                                case 'A':
+                                    huruf = "A";
+                                    break;
+                                case 'B':
+                                    huruf = "B";
+                                    break;
+                                case 'C':
+                                    huruf = "C";
+                                    break;
+                                case 'D':
+                                    huruf = "D";
+                                    break;
+                                case 'E':
+                                    huruf = "E";
+                                    break;
+                                case 'F':
+                                    huruf = "F";
+                                    break;
+                                case 'G':
+                                    huruf = "G";
+                                    break;
+                                case 'H':
+                                    huruf = "H";
+                                    break;
+                                case 'I':
+                                    huruf = "I";
+                                    break;
+                                case 'J':
+                                    huruf = "J";
+                                    break;
+                                case 'K':
+                                    huruf = "K";
+                                    break;
+                            }
+                            for (int i = 1; i <= 22; i++)
+                            {
+                                if (c == 'I' || c == 'O') { break; }
+                                if (i == 12)
+                                {
+                                    if (c == 'A' && i == 12)
+                                    {
+                                        kursi188[count] = (huruf + i);
+                                        count++;
+                                        kursi188[count] = (huruf + (i + 1));
+                                        count++;
+                                    }
+                                    i = 14;
+                                }
+                                if (i == 1 || i == 2)
+                                {
+                                    if ((c == 'A' && cek == true) || (c == 'B' && cek == true) || (c == 'C' && cek == true))
+                                    {
+                                        kursi188[count] = ("A1");
+                                        count++;
+                                        kursi188[count] = ("B1");
+                                        count++;
+                                        kursi188[count] = ("C1");
+                                        count++;
+                                        kursi188[count] = ("A2");
+                                        count++;
+                                        kursi188[count] = ("B2");
+                                        count++;
+                                        kursi188[count] = ("C2");
+                                        count++;
+                                        cek = false;
+                                    }
+                                    i = 3;
+                                }
+                                kursi188[count] = (huruf + i);
+                                count++;
+                            }
+                        }
+                        for (int x = 0; x < kursi188.Length; x++)
+                        {
+                            DBInput.Nomor_Kursi = kursi188[x];
+                            var data = JsonConvert.SerializeObject(DBInput);
+                            var Json = new WebClient();
+                            Json.Headers.Add(HttpRequestHeader.ContentType, "application/json; charset=utf-8");
+                            string response = Json.UploadString(BaseAddress + "Inputdata", data);
+                            Console.WriteLine(response);
+                        }
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Insert Data", "Create Data", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+                MessageBox.Show("Sukses", "Edit Data", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Edit Data", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
         }
 
         private void button13_Click(object sender, EventArgs e)
