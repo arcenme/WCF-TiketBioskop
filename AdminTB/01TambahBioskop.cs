b057f359308b14eab3bbf77abb38f58de67e9585
Ari Suseno <arcen.grizzly@gmail.com>
2018-12-09 13:10:42 +0700

    Layout data film di bioskop

--- AdminTB/01TambahBioskop.cs
+++ AdminTB/01TambahBioskop.cs
@@ -16,12 +16,12 @@ namespace AdminTB
     public partial class _01TambahBioskop : Form
     {
         string BaseAddress = "http://localhost:8080/";
-        string _IDB;
+        string _IDB, _IDFilm;
         public _01TambahBioskop()
         {
             InitializeComponent();
         }
-
+         
         private void _01TambahBioskop_Load(object sender, EventArgs e)
         {
            
@@ -210,11 +210,6 @@ namespace AdminTB
 
         private void KonfirmasiFilm_Click(object sender, EventArgs e)
         {
-
-        }
-
-        private void TambahFilm_Click(object sender, EventArgs e)
-        {
             try
             {
                 DataBioskop DBFilm = new DataBioskop();
@@ -249,37 +244,67 @@ namespace AdminTB
             }
         }
 
+        private void TambahFilm_Click(object sender, EventArgs e)
+        {
+            panelBioskop.Visible = false;
+            panelDataBioskop.Visible = false;
+            panelDataFilm.Visible = false;
+            panelEditBioskop.Visible = false;
+            panelEditFilm.Visible = false;
+            panelFilm.Visible = true;
+            
+        }
+
         private void button4_Click(object sender, EventArgs e)
         {
+            try
+            {
+                var listFilm = listViewFilm.SelectedItems[0];
+                _IDFilm = listFilm.SubItems[0].Text;
 
+                DataBioskop DBBskp = new DataBioskop();
+                DBBskp.IDBioskop = _IDFilm;
+
+                var dataPut = JsonConvert.SerializeObject(DBBskp);
+                var Json = new WebClient();
+                Json.Headers.Add(HttpRequestHeader.ContentType, "application/json; charset=utf-8");
+                string response = Json.UploadString(BaseAddress + "Film/Delete", "DELETE", dataPut);
+                Console.WriteLine(response);
+                MessageBox.Show("Sukses", "Hapus Data", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Hapus Data", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
         }
 
         private void button5_Click(object sender, EventArgs e)
         {
+            panelBioskop.Visible = false;
+            panelDataBioskop.Visible = false;
+            panelDataFilm.Visible = true;
+            panelEditBioskop.Visible = false;
+            panelEditFilm.Visible = false;
+            panelFilm.Visible = false;
             try
             {
-                panelBioskop.Visible = false;
-                panelDataBioskop.Visible = true;
-                panelDataFilm.Visible = false;
-                panelEditBioskop.Visible = true;
-                panelEditFilm.Visible = false;
-                panelFilm.Visible = false;
-               
                 //Get Data
-                var listbskp = listViewBioskop.SelectedItems[0];
-                string IDBioskop = listbskp.SubItems[1].Text;
-                string NamaBioskop = listbskp.SubItems[2].Text;
-                string AlamatBisokop = listbskp.SubItems[3].Text;
-                string Jumlah_Kursi = listbskp.SubItems[4].Text;
-                _IDB = listbskp.SubItems[0].Text;
+                var listFilm = listViewFilm.SelectedItems[0];
+                string judulfilm = listFilm.SubItems[1].Text;
+                string ratting = listFilm.SubItems[2].Text;
+                string aktor = listFilm.SubItems[3].Text;
+                string harga = listFilm.SubItems[4].Text;
+                string poster = listFilm.SubItems[5].Text;
+                _IDFilm = listFilm.SubItems[0].Text;
 
-                panelDataBioskop.Visible = false;
-                panelEditBioskop.Visible = true;
+                panelDataFilm.Visible = false;
+                panelEditFilm.Visible = true;
 
-                EditIDBskp.AppendText(IDBioskop);
-                EditNamaBskp.AppendText(NamaBioskop);
-                EditAlamatBskp.AppendText(AlamatBisokop);
-                EditKapasitasBskp.AppendText(Jumlah_Kursi);
+                textBoxEditJdlFlm.AppendText(judulfilm);
+                textBoxEditRatting.AppendText(ratting);
+                textBoxEditAktor.AppendText(aktor);
+                textBoxEdittHarga.AppendText(harga);
+                TxtBoxEditPoster.AppendText(poster);
             }
             catch (Exception ex)
             {
@@ -289,10 +314,49 @@ namespace AdminTB
 
         private void LihatDataFilm_Click(object sender, EventArgs e)
         {
+            try
+            {
+                panelBioskop.Visible = false;
+                panelFilm.Visible = false;
+                panelDataBioskop.Visible = false;
+                panelDataFilm.Visible = true;
+                listViewFilm.Items.Clear();
 
+                var json = new WebClient().DownloadString(BaseAddress + "Film");
+                var data = JsonConvert.DeserializeObject<List<DataBioskop>>(json);
+                foreach (var film in data)
+                {
+                    ListViewItem dataFilm = new ListViewItem(Convert.ToString(film.IDFilm));
+                    dataFilm.SubItems.Add(film.JudulFilm);
+                    dataFilm.SubItems.Add(film.Ratting);
+                    dataFilm.SubItems.Add(film.Aktor);
+                    dataFilm.SubItems.Add(Convert.ToString(film.Harga));
+                    dataFilm.SubItems.Add(film.Poster);
+                    listViewFilm.Items.Add(dataFilm);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Show Data", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
         }
 
         private void button3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonInput_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button8_Click(object sender, EventArgs e)
         {
 
         }
